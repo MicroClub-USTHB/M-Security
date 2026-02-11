00b37d2a8841d8f51f7d311d5e55446947c359ed
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2026-02-11 14:33:12 +0100

    add flutter_rust_bridge dep

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import m_security
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MSecurityPlugin.register(with: registry.registrar(forPlugin: "MSecurityPlugin"))
+}
